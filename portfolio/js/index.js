e3307a45579dc3afb4670cf2d7778db2beadf263
Nozeil <nozeilwrk@gmail.com>
2022-01-25 19:01:14 +0300

    feat: implement en-ru language change

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,4 +1,4 @@
-'use strict'
+import translateEnRuObj from "./translateEnRu.js";
 const navigation = document.querySelector('.nav');
 const burger = document.querySelector('.burger');
 const transparentBg = document.querySelector('.transparent-bg');
@@ -6,20 +6,22 @@ const portfolioImgs = document.querySelectorAll('.portfolio-image');
 const sectionPortfolioBtns = document.querySelector('.section-portfolio-buttons');
 const portfolioBtns = document.querySelectorAll('.portfolio-button');
 const seasons = ['Winter', 'Spring', 'Summer', 'Autumn'];
+const switchLng = document.querySelector('.switch-lng');
+const lngButtons = document.querySelectorAll('.switch-lng-button');
 
 function toogleMenuClass() {
-	burger.classList.toggle('is-active');
-	navigation.classList.toggle('is-active');
-	transparentBg.classList.toggle('is-active');
+	burger.classList.toggle('menu-is-active');
+	navigation.classList.toggle('menu-is-active');
+	transparentBg.classList.toggle('menu-is-active');
 }
 
 burger.addEventListener('click', toogleMenuClass);
 
 function removeMenuClass(event) {
 	if (event.target.classList.contains('nav-link')) {
-		burger.classList.remove('is-active');
-		navigation.classList.remove('is-active');
-		transparentBg.classList.remove('is-active');
+		burger.classList.remove('menu-is-active');
+		navigation.classList.remove('menu-is-active');
+		transparentBg.classList.remove('menu-is-active');
 	}
 }
 
@@ -27,7 +29,7 @@ navigation.addEventListener('click', removeMenuClass);
 
 function changePortfolioImgs(event) {
 	if (event.target.classList.contains('portfolio-button')) {
-		portfolioImgs.forEach((img, index) => img.src = `assets/img/${event.target.textContent}/portfolio-img-${index + 1}.jpg`
+		portfolioImgs.forEach((img, index) => img.src = `assets/img/${event.target.dataset.i18n}/portfolio-img-${index + 1}.jpg`
 		);
 	}
 }
@@ -36,8 +38,8 @@ sectionPortfolioBtns.addEventListener('click', changePortfolioImgs);
 
 function changePortfolioBtnsClass(event) {
 	if (event.target.classList.contains('portfolio-button')) {
-		portfolioBtns.forEach(btn => btn.classList.remove('active-button'));
-		event.target.classList.add('active-button');
+		portfolioBtns.forEach(btn => btn.classList.remove('active-portfolio-button'));
+		event.target.classList.add('active-portfolio-button');
 	}
 }
 
@@ -52,5 +54,25 @@ function preloadImages() {
 }
 
 preloadImages();
+
+function getTranslate(language) {
+	language = document.querySelector('.active-lng').innerHTML.trim();
+	const dataI18n = document.querySelectorAll('[data-i18n]');
+	dataI18n.forEach(el => {
+		if (el.placeholder) el.placeholder = translateEnRuObj[language][el.dataset.i18n];
+		el.innerHTML = translateEnRuObj[language][el.dataset.i18n];
+	});
+}
+
+function changeLanguageBtnsClass(event) {
+	if (event.target.classList.contains('switch-lng-button')) {
+		lngButtons.forEach(button => button.classList.remove('active-lng'));
+		event.target.classList.add('active-lng');
+	}
+}
+
+switchLng.addEventListener('click',changeLanguageBtnsClass);
+
+switchLng.addEventListener('click', getTranslate);
 
 console.log('1.Вёрстка соответствует макету. Ширина экрана 768px +48.\n2.Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +15.\n3.На ширине экрана 768рх и меньше реализовано адаптивное меню +22.\nTotal 85/85 => 75');
